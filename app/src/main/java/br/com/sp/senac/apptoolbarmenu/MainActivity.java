938e599bfa20edd71e8780d2661b136a55be5c13
tiago.cscamargo <tweedleti@gmail.com>
2020-03-10 12:02:15 -0300

    toolbar

--- app/src/main/java/br/com/sp/senac/apptoolbarmenu/MainActivity.java
+++ app/src/main/java/br/com/sp/senac/apptoolbarmenu/MainActivity.java
@@ -26,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        getSupportActionBar().setTitle("TygolaNoBeat");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
